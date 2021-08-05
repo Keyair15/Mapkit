5279caf7840fcdff9e8f354abd8d55cf88c38739
Keyair15 <87139003+Keyair15@users.noreply.github.com>
2021-08-05 11:00:14 -0500

    Add files via upload

---
+++ Event_app/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  Event_app
+//
+//  Created by Student31 on 7/30/21.
+//
+
+import UIKit
+
+class SecondViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Event_app/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Event_app
+//
+//  Created by Student31 on 7/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func LogIn(_ sender: Any) {
+    }
+    
+}
+
